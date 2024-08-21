d1929a64300ce7dd806f217f9d4593a14be59ef0
Peter Kelly <peter@computer-wizard.com.au>
2024-08-21 14:45:05 +0800

    Addedd resources

---
+++ Campaign/maps/Void/Space3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Space3" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../../tiles/TileSet v1.0.png" width="320" height="320"/>
+</tileset>
---
+++ Campaign/maps/Void/tileset_sf.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tileset_sf" tilewidth="32" tileheight="32" tilecount="2040" columns="60">
+ <image source="../../tiles/TileMap_SF/tileset_sf.png" width="1920" height="1088"/>
+</tileset>
